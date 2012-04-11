aab75cc68af141fca3c425c5252ee87940f36e17
Dan Higham <dan.higham@gmail.com>
2012-04-11 12:00:41 +0100

    Added ability to lookup multiple addresses..

--- lib/postcode_anywhere/lookup.rb
+++ lib/postcode_anywhere/lookup.rb
@@ -1,18 +1,55 @@
 require "cgi"
 module PostcodeAnywhere
   
-  SERVICE_ADDRESS = "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/RetrieveByPostcodeAndBuilding/v1.00/xmle.ws"
+  SERVICE_ADDRESS = "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive"
   
+  BY_POST_CODE_AND_BUILDING = "/RetrieveByPostcodeAndBuilding/v1.00/xmle.ws"
+  BY_POST_CODE = "/Find/v1.10/xmla.ws"
+
   include HTTParty
   format :xml
   
   class Lookup
+
     def self.lookup(options)
-      results = PostcodeAnywhere.get(SERVICE_ADDRESS+"?Key=#{PostcodeAnywhere.key}&Postcode=#{CGI::escape(options[:postcode])}&Building=#{options[:number]}")
-      address = Address.new
-      address.line1 = results["Table"]["Row"]["Line1"]
-      address.company = results["Table"]["Row"]["Company"]
-      address
+
+      url = SERVICE_ADDRESS + BY_POST_CODE
+      url = "#{url}?Key=#{PostcodeAnywhere.key}&SearchTerm=#{CGI::escape(options[:postcode])}"
+
+      if not options[:number].nil? and not options[:postcode].nil?
+        url = SERVICE_ADDRESS + BY_POST_CODE_AND_BUILDING 
+        url = "#{url}?Key=#{PostcodeAnywhere.key}&Postcode=#{CGI::escape(options[:postcode])}&Building=#{options[:number]}"
+      end
+      results = PostcodeAnywhere.get url
+
+      multiple_results = !results["Table"]["Rows"].nil?
+
+      if multiple_results
+
+        addresses = []
+
+        results["Table"]["Rows"]["Row"].each do |x|
+
+          address = Address.new
+
+          address.line1 = x["StreetAddress"]
+          address.company = x["Company"]
+
+          addresses << address
+
+        end
+
+        addresses
+
+      else
+        
+        address = Address.new
+
+        address.line1 = results["Table"]["Row"]["Line1"]
+        address.company = results["Table"]["Row"]["Company"]
+        address
+
+      end
     end
   end
 end
--- spec/postcode_anywhere_spec.rb
+++ spec/postcode_anywhere_spec.rb
@@ -7,6 +7,11 @@ describe PostcodeAnywhere do
     FakeWeb.register_uri(:get, "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/RetrieveByPostcodeAndBuilding/v1.00/xmle.ws?Key=AAAA-BBBB-CCCC-DDDD&Postcode=SW1X+7XL&Building=87", :body => success_file, :content_type => "application/xml")
   }
   
+  let(:fake_successful_multiple_request) {
+    success_file = File.open(File.expand_path("../fixtures/multiple-results.xml", __FILE__)).read
+    FakeWeb.register_uri(:get, "http://services.postcodeanywhere.co.uk/PostcodeAnywhere/Interactive/Find/v1.10/xmla.ws?Key=AAAA-BBBB-CCCC-DDDD&SearchTerm=IP12+1SA", :body => success_file, :content_type => "application/xml")
+  }
+
   context "Invalid API credentials provided" do
     it "should raise if not API credientials are provided" do
       expect { PostcodeAnywhere.lookup(:number => 87, :postcode => "SW1X 7XL") }.to raise_error
@@ -28,6 +33,19 @@ describe PostcodeAnywhere do
       address = PostcodeAnywhere.lookup(:number => 87, :postcode => "SW1X 7XL")
       address.company.should == "Harrods"
       address.line1.should == "87-135 Brompton Road"
+    end
+
+    it "should return multiple addresses if only a postcode is provided" do
+      fake_successful_multiple_request
+      addresses = PostcodeAnywhere.lookup(:postcode => "IP12 1SA")
+
+      addresses.class.should == Array
+      addresses.length.should == 15
+
+      addresses.each do |address|
+        address.line1.class.should == String
+      end
+
     end
   end
   
